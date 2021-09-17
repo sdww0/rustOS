fb04359a087c1c565b50ccf677a028ecd2b0819f
sdww0 <60682162+sdww0@users.noreply.github.com>
2021-09-17 06:14:01 -0700

    Add files via upload
    init

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use blog_os::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    blog_os::init();
+
+    // trigger a page fault
+    // unsafe {
+    //     *(0xdeadbeef as *mut u64) = 42;
+    // };
+
+    // as before
+    #[cfg(test)]
+    test_main();
+
+
+    println!("It did not crash!");
+    blog_os::hlt_loop();            // new
+}
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    blog_os::hlt_loop();   
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
